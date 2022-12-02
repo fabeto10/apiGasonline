e033aa7eda6709f5113336b9b26b230af90f2b1c
fabeto10 <f0414475@gmail.com>
2022-12-02 14:18:55 -0400

    fix

--- server/adminBro.js
+++ server/adminBro.js
@@ -1,33 +1,33 @@
-const AdminBro = require( 'admin-bro');
-const AdminBroExpress = require('@admin-bro/express');
-const AdminBroMongoose = require('@admin-bro/mongoose')
+// const AdminBro = require( 'admin-bro');
+// const AdminBroExpress = require('@admin-bro/express');
+// const AdminBroMongoose = require('@admin-bro/mongoose')
 
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const adminBro = new AdminBro ({
-    Databases: [],
-    rootPath: '/admin',
-});
+// const adminBro = new AdminBro ({
+//     Databases: [],
+//     rootPath: '/admin',
+// });
 
-const run = async () => {
-    const connection = mongoose.connect('mongodb://localhost:27017/test', {
-        useNewUrlParser: true,
-    });
-    new AdminBro ({
-        Database: [connection]
-    });
-};
-run();
-
-
-const router = AdminBroExpress.buildRouter (adminBro)
+// const run = async () => {
+//     const connection = mongoose.connect('mongodb://localhost:27017/test', {
+//         useNewUrlParser: true,
+//     });
+//     new AdminBro ({
+//         Database: [connection]
+//     });
+// };
+// run();
 
 
-AdminBro.registerAdapter(AdminBroMongoose)
+// const router = AdminBroExpress.buildRouter (adminBro)
 
-app.use(adminBro.options.rootPath, router)
-app.listen(8080, () => console.log('AdminBro is under localhost:8080/admin'))
+
+// AdminBro.registerAdapter(AdminBroMongoose)
+
+// app.use(adminBro.options.rootPath, router)
+// app.listen(8080, () => console.log('AdminBro is under localhost:8080/admin'))
 
--- server/server.js
+++ server/server.js
@@ -1,31 +1,36 @@
 require('./config/config');
+require('express-async-errors')
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
-const AdminBro = require('admin-bro');
-const AdminBroExpress = require('@admin-bro/express');
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
+const adminRouter = require('./admin/admin.router');
 
+// parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(express.json());
+app.use(bodyParser.json());
 // Configuracion global de rutas
 app.use(require('./users/routes/index'));
 app.use(require('./placas/routes/index'));
+app.use('/admin', adminRouter);
 
 let renderHTML = path.resolve(__dirname, '../public/index.html');
 
-app.get('/', function (req, res) {
-    res.sendFile(renderHTML);
-});
+app.get('/', (req, res) => res.send('Hello World!'))
 
-mongoose.connect(process.env.URLDB, {}, (err) => {
-    if (err) throw err;
-    console.log("Base de datos online");
-});
+const run = async () => {
+    await mongoose.connect(process.env.URLDB, {}, (err) => {
+        if (err) throw err;
+        console.log("Base de datos online");
+    });
+    
+    await app.listen(process.env.PORT, ()=> {
+        console.log("Escuchando en puerto 3000");
+    });
+};
 
-app.listen(process.env.PORT, ()=> {
-    console.log("Escuchando en puerto 3000");
-});
+run();
+
